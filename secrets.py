c5cb71c464fcfedf6930bce4410a53f620c9e1e5
Rahul Arya <rahularya@berkeley.edu>
2019-08-06 03:19:44 -0700

    Update secrets.py
    Wow I'm dumb

--- secrets.py
+++ secrets.py
@@ -2,4 +2,4 @@
 To get an access token, run `python ok --get-token` and paste the return value into the below string
 """
 
-ACCESS_TOKEN = "LoUVXLOm7nf0BbDfj5MpbuEVC4mt1M"
+ACCESS_TOKEN = "YOUR TOKEN HERE!!!!"
